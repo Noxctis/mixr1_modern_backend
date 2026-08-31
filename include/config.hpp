d9c4dc1b94fd8c642255faf275dda81f397a0d6c
Noxctis <21101819@usc.edu.ph>
2026-08-31 17:01:12 +0800

    testing 256 ppr

--- include/config.hpp
+++ include/config.hpp
@@ -5,7 +5,7 @@
 
 namespace Config {
     // --- Kinematics & DSP ---
-    constexpr double ENCODER_CPR = 800.0;               
+    constexpr double ENCODER_CPR = 1024.0;               
     constexpr double RPM_ALPHA = 0.15;
     constexpr size_t SMA_WINDOW_SIZE = 8;                
     constexpr int DEADBAND_TICK_THRESHOLD = 2;           
